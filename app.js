e2ed9b8c3c3d0d5fde6f65d080715f187b4153e0
vise1350 <125251867+vise1350@users.noreply.github.com>
2023-09-18 20:13:37 +0530

    task2

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express=require("express")
 
-
-
 const app=express()
 
-app.use((req,res,next)=>{
-    console.log("Middleware added")
-    next();
+const bodyParser=require("body-parser")
 
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use("/add-product",(req,res,next)=>{
+
+    res.send(`<form action='/product' method='POST'>
+    <label > Name:</label>
+     <input type='text' name='title' label='name'> <br>
+     <label> Size</label>
+     <input type='text' name='size'> <br>
+     
+         <button type='submit'>Add product
+          </button>
+         
+          
+          </input>  
+           </form>`)
+ 
+    // res.send("<h2> Welcome to add product page </h2>");
+
+    
 })
-app.use((req,res,next)=>{
-    console.log("Another Middleware added")
-    // res.send({key1:value})
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/")
+})
+
+app.use("/",(req,res,next)=>{
+  
     res.send("<h1>Hello from expressJS </h1>")
 
 })
 app.listen(3000)
+console.log("server is running successfully")
    
